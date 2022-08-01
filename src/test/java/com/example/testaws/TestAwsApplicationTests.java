80e8bcf7211901431413a03da748769bb689c983
고유경 <youkyoung71@naver.com>
2022-08-01 14:47:26 +0900

    first commit

---
+++ src/test/java/com/example/testaws/TestAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testaws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAwsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
